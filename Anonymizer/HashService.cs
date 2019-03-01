da936b4603e1f47d63ecebaa2f44dee5d05de314
ZakFahey <z.fahey@gmail.com>
2019-03-01 12:11:13 -0500

    Fixed comment

--- Anonymizer/HashService.cs
+++ Anonymizer/HashService.cs
@@ -15,7 +15,7 @@ namespace Anonymizer
 
         public string Hash(string plaintext)
         {
-            // We are cutting off the hash from the result because we do not want nor need it. You can now only crack any data if you have the hash yourself.
+            // We are cutting off the salt from the result because we do not want nor need it. You can now only crack any data if you have the hash yourself.
             // Prevents non-server people from looking up hashes for known usernames and checking for a relation.
             return BCrypt.Net.BCrypt.HashPassword(plaintext, SecretSalt).Substring(SecretSalt.Length);
         }
